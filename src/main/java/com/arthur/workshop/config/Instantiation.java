65a41b8fd45bcdcdf8d47b186dc8a5c4a90bce80
Arthur Gabriel <arthurelgg@gmail.com>
2022-09-09 18:37:26 -0300

    Referenciando os posts do usuario

--- src/main/java/com/arthur/workshop/config/Instantiation.java
+++ src/main/java/com/arthur/workshop/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.arthur.workshop.config;
 
 import com.arthur.workshop.domain.Post;
 import com.arthur.workshop.domain.User;
+import com.arthur.workshop.dto.AuthorDTO;
 import com.arthur.workshop.repository.PostRepository;
 import com.arthur.workshop.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -36,10 +37,15 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1,post2));
+        userRepository.save(maria);
+
     }
 }

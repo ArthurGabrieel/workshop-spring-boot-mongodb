474604e34c278467169a5e691e00927bb78faa24
Arthur Gabriel <arthurelgg@gmail.com>
2022-09-09 19:13:01 -0300

    Acrescenta comentarios nos posts e query methods

--- src/main/java/com/arthur/workshop/config/Instantiation.java
+++ src/main/java/com/arthur/workshop/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.arthur.workshop.config;
 import com.arthur.workshop.domain.Post;
 import com.arthur.workshop.domain.User;
 import com.arthur.workshop.dto.AuthorDTO;
+import com.arthur.workshop.dto.CommentDTO;
 import com.arthur.workshop.repository.PostRepository;
 import com.arthur.workshop.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/arthur/workshop/resources/PostResource.java
+++ src/main/java/com/arthur/workshop/resources/PostResource.java
@@ -1,12 +1,12 @@
 package com.arthur.workshop.resources;
 
 import com.arthur.workshop.domain.Post;
+import com.arthur.workshop.resources.util.URL;
 import com.arthur.workshop.services.PostService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -24,4 +24,11 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
 }

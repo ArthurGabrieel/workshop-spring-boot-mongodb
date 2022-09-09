1ab33fd642cf0e1bc7f25691b032b09daed2abb9
Arthur Gabriel <arthurelgg@gmail.com>
2022-09-09 16:25:58 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/arthur/workshop/services/UserService.java
@@ -0,0 +1,21 @@
+package com.arthur.workshop.services;
+
+import com.arthur.workshop.domain.User;
+import com.arthur.workshop.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}

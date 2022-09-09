237abf3a3e2b4264436a7ecc5625e4a10c4f4b7d
Arthur Gabriel <arthurelgg@gmail.com>
2022-09-09 17:12:06 -0300

    Insercao de usuario com post

--- src/main/java/com/arthur/workshop/services/UserService.java
+++ src/main/java/com/arthur/workshop/services/UserService.java
@@ -1,6 +1,7 @@
 package com.arthur.workshop.services;
 
 import com.arthur.workshop.domain.User;
+import com.arthur.workshop.dto.UserDTO;
 import com.arthur.workshop.repository.UserRepository;
 import com.arthur.workshop.services.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
